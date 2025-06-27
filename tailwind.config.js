38d6d2fe56061a43bf9410735e437f06b99654d8
KevinHegg <kevinhegg@gmail.com>
2025-06-27 15:59:07 -0400

    fix: place Tailwind and PostCSS configs in correct files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
